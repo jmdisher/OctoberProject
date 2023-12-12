670b5ada6ec58fc03985aeb9d99085acb972d8aa
Jeff Disher <jeff@jeffdisher.com>
2023-12-12 05:29:28 -0500

    Added a MessageQueue for inter-thread communication
    -this is important for some later changes where notifications from components running in other threads need to be interpreted in a strictly in-order part of the system

---
+++ src/com/jeffdisher/october/utils/MessageQueue.java
@@ -0,0 +1,95 @@
+package com.jeffdisher.october.utils;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+/**
+ * A blocking queue of Runnable objects for use in message passing between threads.
+ */
+public class MessageQueue
+{
+	private Queue<Runnable> _queue = new LinkedList<>();
+	private boolean _running = true;
+
+	/**
+	 * Polls for the next runnable, blocking until one exists, the millisToWait have expired, or the queue is shut down.
+	 * Note that this will return null on shutdown, even if there are still Runnable objects in the queue.
+	 * 
+	 * @param millisToWait The time to wait, in milliseconds, before returning timeoutRunnable (ignored if null
+	 * timeoutRunnable).  This must always be >= 0L (even if timeoutRunnable is null) and > 0L if timeoutRunnable is
+	 * non-null.
+	 * @param timeoutRunnable The runnable to return if the timeout elapses without anything else happening.
+	 * @return The next Runnable or null, if the queue is shut down.
+	 */
+	public synchronized Runnable pollForNext(long millisToWait, Runnable timeoutRunnable)
+	{
+		// Millis to wait must be a reasonable value.
+		if (null != timeoutRunnable)
+		{
+			Assert.assertTrue(millisToWait > 0L);
+		}
+		else
+		{
+			Assert.assertTrue(millisToWait >= 0L);
+		}
+		
+		// Determine whether or not to honour the timeout.
+		long realWaitMillis = (null != timeoutRunnable)
+				? millisToWait
+				: 0L
+		;
+		// We want to break out if we wanted a timeout and it expired so we use a local flag.
+		boolean continueToWait = true;
+		while (_running && continueToWait && _queue.isEmpty())
+		{
+			try
+			{
+				this.wait(realWaitMillis);
+				if (realWaitMillis > 0L)
+				{
+					// If we were waiting with a timeout, we want to drop out now (either the timer expired or we were notified of new data).
+					continueToWait = false;
+				}
+			}
+			catch (InterruptedException e)
+			{
+				// We don't use interruption.
+				throw Assert.unexpected(e);
+			}
+		}
+		return _running
+				? (!_queue.isEmpty()
+						? _queue.remove()
+						: timeoutRunnable
+				)
+				: null
+		;
+	}
+ 
+	/**
+	 * Enqueues the next runnable task.
+	 * 
+	 * @param r The runnable task.
+	 * @return True if this was enqueued, false if the receiver has been shut down.
+	 */
+	public synchronized boolean enqueue(Runnable r)
+	{
+		if (_running)
+		{
+			_queue.add(r);
+			this.notify();
+		}
+		return _running;
+	}
+
+	/**
+	 * Shuts down the queue.  Note that this will cause future calls to enqueue() to fail and will allow any threads
+	 * blocked in pollForNext() to return null.
+	 */
+	public synchronized void shutdown()
+	{
+		_running = false;
+		this.notifyAll();
+	}
+}
---
+++ test/com/jeffdisher/october/utils/TestMessageQueue.java
@@ -0,0 +1,101 @@
+package com.jeffdisher.october.utils;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestMessageQueue
+{
+	@Test
+	public void startStop() throws Throwable
+	{
+		// Start and stop the queue without doing anything, on one thread.
+		MessageQueue queue = new MessageQueue();
+		queue.shutdown();
+		Assert.assertNull(queue.pollForNext(0L, null));
+	}
+
+	@Test
+	public void basicConsumer() throws Throwable
+	{
+		MessageQueue queue = new MessageQueue();
+		Thread thread = new Thread(() -> {
+			Runnable r = queue.pollForNext(0L, null);
+			while (null != r)
+			{
+				r.run();
+				r = queue.pollForNext(0L, null);
+			}
+		});
+		thread.start();
+		
+		int count[] = new int[1];
+		for (int i = 0; i < 10; ++i)
+		{
+			queue.enqueue(() -> {
+				count[0] += 1;
+			});
+		}
+		CyclicBarrier barrier = new CyclicBarrier(2);
+		queue.enqueue(() -> {
+			try
+			{
+				barrier.await();
+			}
+			catch (InterruptedException | BrokenBarrierException e)
+			{
+				// Not expected.
+				Assert.fail();
+			}
+		});
+		barrier.await();
+		queue.shutdown();
+		thread.join();
+		Assert.assertEquals(10, count[0]);
+	}
+
+	@Test
+	public void testWithTimeout() throws Throwable
+	{
+		// Pre-populate the queue and then add a timeout runnable to await on the barrier - it should only run when everything else is done.
+		MessageQueue queue = new MessageQueue();
+		int count[] = new int[1];
+		for (int i = 0; i < 10; ++i)
+		{
+			queue.enqueue(() -> {
+				count[0] += 1;
+			});
+		}
+		
+		CyclicBarrier barrier = new CyclicBarrier(2);
+		Runnable timeout = () -> {
+			try
+			{
+				barrier.await();
+			}
+			catch (InterruptedException | BrokenBarrierException e)
+			{
+				// Not expected.
+				Assert.fail();
+			}
+		};
+		
+		Thread thread = new Thread(() -> {
+			Runnable r = queue.pollForNext(1L, timeout);
+			while (null != r)
+			{
+				r.run();
+				r = queue.pollForNext(1L, timeout);
+			}
+		});
+		thread.start();
+		
+		barrier.await();
+		queue.shutdown();
+		thread.join();
+		Assert.assertEquals(10, count[0]);
+	}
+}
