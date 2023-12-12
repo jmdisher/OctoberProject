5ec2380e01c49f974b8021d3c17767d21e80b209
Jeff Disher <jeff@jeffdisher.com>
2023-12-12 16:33:21 -0500

    Added an integration test of ServerRunner and ClientRunner
    -this is to demonstrate how the system would function as a complete system, just with a faked-up network and no user interface

---
+++ test/com/jeffdisher/october/integration/ConnectionFabric.java
@@ -0,0 +1,165 @@
+package com.jeffdisher.october.integration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+
+import com.jeffdisher.october.changes.IEntityChange;
+import com.jeffdisher.october.client.IClientAdapter;
+import com.jeffdisher.october.data.IReadOnlyCuboidData;
+import com.jeffdisher.october.mutations.IMutation;
+import com.jeffdisher.october.server.IServerAdapter;
+import com.jeffdisher.october.server.ServerRunner;
+import com.jeffdisher.october.types.Entity;
+
+
+/**
+ * Used by integration tests which want to connect clients to a server.
+ * All calls from one are issued directly through to the other (since their listener interfaces assume the calling
+ * thread is borrowed and should return without doing heavy work).
+ */
+public class ConnectionFabric implements IServerAdapter
+{
+	public int nextClientId = 1;
+
+	private IServerAdapter.IListener _server;
+	private Map<Integer, IClientAdapter.IListener> _clients = new HashMap<>();
+	private Map<Integer, Long> _tickObservedByClient = new HashMap<>(Map.of(ServerRunner.FAKE_CLIENT_ID, 0L));
+
+	// ---------------------------------- Server adapter.
+	@Override
+	public synchronized void readyAndStartListening(IServerAdapter.IListener listener)
+	{
+		Assert.assertNull(_server);
+		_server = listener;
+		this.notifyAll();
+	}
+
+	@Override
+	public synchronized void sendEntity(int clientId, Entity entity)
+	{
+		_clients.get(clientId).receivedEntity(entity);
+	}
+
+	@Override
+	public synchronized void sendCuboid(int clientId, IReadOnlyCuboidData cuboid)
+	{
+		_clients.get(clientId).receivedCuboid(cuboid);
+	}
+
+	@Override
+	public synchronized void sendChange(int clientId, int entityId, IEntityChange change)
+	{
+		_clients.get(clientId).receivedChange(entityId, change);
+	}
+
+	@Override
+	public synchronized void sendMutation(int clientId, IMutation mutation)
+	{
+		_clients.get(clientId).receivedMutation(mutation);
+	}
+
+	@Override
+	public synchronized void sendEndOfTick(int clientId, long tickNumber, long latestLocalCommitIncluded, long latestLocalActivityIncluded)
+	{
+		if (ServerRunner.FAKE_CLIENT_ID != clientId)
+		{
+			_clients.get(clientId).receivedEndOfTick(tickNumber, latestLocalCommitIncluded, latestLocalActivityIncluded);
+		}
+		_tickObservedByClient.put(clientId, tickNumber);
+		this.notifyAll();
+	}
+
+	@Override
+	public synchronized void disconnectClient(int clientId)
+	{
+		_clients.remove(clientId);
+	}
+
+	public synchronized int connectAndStartListening(IClientAdapter.IListener listener)
+	{
+		int thisClient = this.nextClientId;
+		this.nextClientId += 1;
+		
+		_clients.put(thisClient, listener);
+		listener.adapterConnected(thisClient);
+		_tickObservedByClient.put(thisClient, 0L);
+		_server.clientConnected(thisClient);
+		this.notifyAll();
+		return thisClient;
+	}
+
+	public synchronized void disconnect(int clientId)
+	{
+	}
+
+	public synchronized void sendChange(int clientId, IEntityChange change, long commitLevel, boolean isMultiPhase)
+	{
+		_server.changeReceived(clientId, change, commitLevel, isMultiPhase);
+	}
+
+	public synchronized void waitForServer() throws InterruptedException
+	{
+		while (null == _server)
+		{
+			this.wait();
+		}
+	}
+
+	public synchronized void waitForClient(int clientId) throws InterruptedException
+	{
+		while(!_clients.containsKey(clientId))
+		{
+			this.wait();
+		}
+	}
+
+	public synchronized long getLatestTick(int clientId) throws InterruptedException
+	{
+		return _tickObservedByClient.get(clientId);
+	}
+
+	public synchronized void waitForTick(int clientId, long tickNumber) throws InterruptedException
+	{
+		while(_tickObservedByClient.get(clientId) < tickNumber)
+		{
+			this.wait();
+		}
+	}
+
+	/**
+	 * A factory method to create a new client bound to the server on the receiver.
+	 * 
+	 * @return A new per-client adapter.
+	 */
+	public PerClient newClient()
+	{
+		return new PerClient();
+	}
+
+
+	/**
+	 * The per-client adapter created when connecting a new client to this test network.
+	 */
+	public class PerClient implements IClientAdapter
+	{
+		public int clientId = 0;
+		
+		@Override
+		public void connectAndStartListening(IListener listener)
+		{
+			this.clientId = ConnectionFabric.this.connectAndStartListening(listener);
+		}
+		@Override
+		public void disconnect()
+		{
+			ConnectionFabric.this.disconnect(this.clientId);
+		}
+		@Override
+		public void sendChange(IEntityChange change, long commitLevel, boolean isMultiPhase)
+		{
+			ConnectionFabric.this.sendChange(this.clientId, change, commitLevel, isMultiPhase);
+		}
+	}
+}
---
+++ test/com/jeffdisher/october/integration/TestIntegrationRunners.java
@@ -0,0 +1,181 @@
+package com.jeffdisher.october.integration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jeffdisher.october.changes.EntityChangeMove;
+import com.jeffdisher.october.client.ClientRunner;
+import com.jeffdisher.october.client.SpeculativeProjection;
+import com.jeffdisher.october.client.ClientRunner.IListener;
+import com.jeffdisher.october.data.CuboidData;
+import com.jeffdisher.october.data.IOctree;
+import com.jeffdisher.october.data.IReadOnlyCuboidData;
+import com.jeffdisher.october.data.OctreeObject;
+import com.jeffdisher.october.data.OctreeShort;
+import com.jeffdisher.october.registries.ItemRegistry;
+import com.jeffdisher.october.server.ServerRunner;
+import com.jeffdisher.october.types.CuboidAddress;
+import com.jeffdisher.october.types.Entity;
+import com.jeffdisher.october.types.EntityLocation;
+
+
+public class TestIntegrationRunners
+{
+	@Test
+	public void singleClientJoin() throws Throwable
+	{
+		// We want to create a server with a single cuboid, connect a client to it, and observe that the client sees everything.
+		ConnectionFabric fabric = new ConnectionFabric();
+		ServerRunner server = new ServerRunner(100L, fabric, () -> System.currentTimeMillis());
+		fabric.waitForServer();
+		
+		// Create and load the cuboid full of stone with no inventories.
+		OctreeShort blockData = OctreeShort.create(ItemRegistry.STONE.number());
+		OctreeObject inventoryData = OctreeObject.create();
+		CuboidData cuboid = CuboidData.createNew(new CuboidAddress((short)0, (short)0, (short)0), new IOctree[] { blockData, inventoryData });
+		server.loadCuboid(cuboid);
+		// We want to wait a tick for this cuboid to be picked up.
+		fabric.waitForTick(0, fabric.getLatestTick(0) + 1L);
+		
+		// Create a client and connect them to the server.
+		ClientListener listener = new ClientListener();
+		ClientRunner client = new ClientRunner(fabric.newClient(), listener, listener);
+		// We know that this first client will get ID 1.
+		int clientId1 = 1;
+		fabric.waitForClient(clientId1);
+		client.runPendingCalls(System.currentTimeMillis());
+		
+		// Wait until we see ourself and the cuboid where we are standing (we expect this within 4 ticks).
+		fabric.waitForTick(clientId1, fabric.getLatestTick(clientId1) + 4L);
+		client.runPendingCalls(System.currentTimeMillis());
+		Assert.assertNotNull(listener.entities.get(1));
+		Assert.assertNotNull(listener.cuboids.get(cuboid.getCuboidAddress()));
+		
+		// We are done.
+		server.shutdown();
+	}
+
+	@Test
+	public void twoClientsMove() throws Throwable
+	{
+		// We want to create a server with a single cuboid, connect a client to it, and observe that the client sees everything.
+		ConnectionFabric fabric = new ConnectionFabric();
+		ServerRunner server = new ServerRunner(100L, fabric, () -> System.currentTimeMillis());
+		fabric.waitForServer();
+		
+		// Create and load the cuboid full of stone with no inventories.
+		OctreeShort blockData = OctreeShort.create(ItemRegistry.STONE.number());
+		OctreeObject inventoryData = OctreeObject.create();
+		CuboidData cuboid = CuboidData.createNew(new CuboidAddress((short)0, (short)0, (short)0), new IOctree[] { blockData, inventoryData });
+		server.loadCuboid(cuboid);
+		// We want to wait a tick for this cuboid to be picked up.
+		fabric.waitForTick(0, fabric.getLatestTick(0) + 1L);
+		
+		// Create the first client.
+		ClientListener listener1 = new ClientListener();
+		ClientRunner client1 = new ClientRunner(fabric.newClient(), listener1, listener1);
+		// We know that this first client will get ID 1.
+		int clientId1 = 1;
+		fabric.waitForClient(clientId1);
+		client1.runPendingCalls(System.currentTimeMillis());
+		
+		// Create the second client.
+		ClientListener listener2 = new ClientListener();
+		ClientRunner client2 = new ClientRunner(fabric.newClient(), listener2, listener2);
+		int clientId2 = 2;
+		fabric.waitForClient(clientId2);
+		client2.runPendingCalls(System.currentTimeMillis());
+		
+		// Wait until both of the clients have observed both entities for the first time.
+		while (2 != listener1.entities.size())
+		{
+			fabric.waitForTick(clientId1, fabric.getLatestTick(clientId1) + 1L);
+			client1.runPendingCalls(System.currentTimeMillis());
+		}
+		while (2 != listener2.entities.size())
+		{
+			fabric.waitForTick(clientId2, fabric.getLatestTick(clientId2) + 1L);
+			client2.runPendingCalls(System.currentTimeMillis());
+		}
+		
+		// Both of these clients need to move and then we can verify that both sides can see the correct outcome.
+		EntityLocation location1 = new EntityLocation(1.0f, 1.0f, 1.0f);
+		EntityLocation location2 = new EntityLocation(-1.0f, -1.0f, -1.0f);
+		client1.applyLocalChange(new EntityChangeMove(location1), false, System.currentTimeMillis());
+		client2.applyLocalChange(new EntityChangeMove(location2), false, System.currentTimeMillis());
+		// Note that we only send out pending updates from a client once we receive a commit (current implementation, at least).
+		fabric.waitForTick(clientId2, fabric.getLatestTick(clientId2) + 1L);
+		client1.runPendingCalls(System.currentTimeMillis());
+		client2.runPendingCalls(System.currentTimeMillis());
+		
+		// Wait until we see ourself and the cuboid where we are standing (we expect this within 4 ticks).
+		fabric.waitForTick(clientId2, fabric.getLatestTick(clientId2) + 4L);
+		client1.runPendingCalls(System.currentTimeMillis());
+		client2.runPendingCalls(System.currentTimeMillis());
+		Assert.assertEquals(location1, listener1.entities.get(clientId1).location());
+		Assert.assertEquals(location2, listener1.entities.get(clientId2).location());
+		Assert.assertEquals(location1, listener2.entities.get(clientId1).location());
+		Assert.assertEquals(location2, listener2.entities.get(clientId2).location());
+		
+		// We are done.
+		server.shutdown();
+	}
+
+
+	private final static class ClientListener implements SpeculativeProjection.IProjectionListener, IListener
+	{
+		public final Map<CuboidAddress, IReadOnlyCuboidData> cuboids = new HashMap<>();
+		public final Map<Integer, Entity> entities = new HashMap<>();
+		
+		// SpeculativeProjection.IProjectionListener
+		@Override
+		public void cuboidDidLoad(IReadOnlyCuboidData cuboid)
+		{
+			Object old = this.cuboids.put(cuboid.getCuboidAddress(), cuboid);
+			Assert.assertNull(old);
+		}
+		@Override
+		public void cuboidDidChange(IReadOnlyCuboidData cuboid)
+		{
+			Object old = this.cuboids.put(cuboid.getCuboidAddress(), cuboid);
+			Assert.assertNotNull(old);
+		}
+		@Override
+		public void cuboidDidUnload(CuboidAddress address)
+		{
+			Object old = this.cuboids.remove(address);
+			Assert.assertNotNull(old);
+		}
+		@Override
+		public void entityDidLoad(Entity entity)
+		{
+			Object old = this.entities.put(entity.id(), entity);
+			Assert.assertNull(old);
+		}
+		@Override
+		public void entityDidChange(Entity entity)
+		{
+			Object old = this.entities.put(entity.id(), entity);
+			Assert.assertNotNull(old);
+		}
+		@Override
+		public void entityDidUnload(int id)
+		{
+			Object old = this.entities.remove(id);
+			Assert.assertNotNull(old);
+		}
+		
+		// IListener
+		@Override
+		public void clientDidConnectAndLogin(int assignedLocalEntityId)
+		{
+		}
+		@Override
+		public void clientDisconnected()
+		{
+		}
+	}
+}
