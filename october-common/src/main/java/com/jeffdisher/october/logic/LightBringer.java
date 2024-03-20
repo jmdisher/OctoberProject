9e4fc4acf89d913860c35674a197731e8334b191
Jeff Disher <jeff@jeffdisher.com>
2024-03-20 12:38:37 -0400

    Added the light propagation algorithm
    -note that this is just a breadth-first flood-fill which we may want to replace in the future (since this flows around corners instead of casting a shadow)
    -this isn't yet used by the rest of the code as the details of how to fit this into the parallel executor model and how block updates should be managed is yet to be finalized

---
+++ october-common/src/main/java/com/jeffdisher/october/logic/LightBringer.java
@@ -0,0 +1,310 @@
+package com.jeffdisher.october.logic;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.function.Function;
+
+import com.jeffdisher.october.aspects.LightAspect;
+import com.jeffdisher.october.data.IBlockProxy;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.Block;
+import com.jeffdisher.october.types.CraftOperation;
+import com.jeffdisher.october.types.FuelState;
+import com.jeffdisher.october.types.Inventory;
+import com.jeffdisher.october.utils.Assert;
+
+
+/**
+ * Utility class to manage light propagation.  The caller is responsible for making sure that this is called at the
+ * right time for whatever operation it is performing.
+ * Note that the implementation is a breadth-first flood-fill sort of approach.  This also means that light can actually
+ * spill around corners (it can solve mazes), which may not be ideal but is our current approach.
+ */
+public class LightBringer
+{
+	/**
+	 * Called to spread light from a new source which has just been placed in the world (assumes that the start location
+	 * already has startLevel light level set in the block).
+	 * Note that this doesn't change the underlying data, only returns the changes which must be made.
+	 * 
+	 * @param lookup A function to look up block proxies.
+	 * @param start The starting light source location.
+	 * @param startLevel The intensity of the new light source.
+	 * @return A map of locations and light levels to update.
+	 */
+	public static Map<AbsoluteLocation, Short> spreadLight(Function<AbsoluteLocation, IBlockProxy> lookup, AbsoluteLocation start, short startLevel)
+	{
+		// We need to have something to do.
+		Assert.assertTrue(startLevel > 1);
+		Assert.assertTrue(startLevel <= LightAspect.MAX_LIGHT);
+		Map<AbsoluteLocation, Short> updates = new HashMap<>();
+		updates.put(start, startLevel);
+		
+		Queue<_Step> queue = new LinkedList<>();
+		
+		// We assume that the starting block has already been set.
+		Assert.assertTrue(lookup.apply(start).getLight() == startLevel);
+		
+		// Enqueue the surrounding blocks.
+		_enqueueLightNeighbours(queue, updates, lookup, start, startLevel);
+		
+		_runLightQueue(queue, updates, lookup);
+		
+		// Remove the original since it was just part of a termination condition.
+		updates.remove(start);
+		return updates;
+	}
+
+	/**
+	 * Called to remove the light spread from a now-removed source in the world (assumes that the start location already
+	 * has its light level set to 0 in the block).  This also takes into account the re-flowing of other light sources
+	 * which still remain after removing this one.
+	 * Note that this doesn't change the underlying data, only returns the changes which must be made.
+	 * 
+	 * @param lookup A function to look up block proxies.
+	 * @param start The location where the light source was removed.
+	 * @param previousLevel The previous value of the light source, prior to its removal.
+	 * @return A map of locations and light levels to update.
+	 */
+	public static Map<AbsoluteLocation, Short> removeLight(Function<AbsoluteLocation, IBlockProxy> lookup, AbsoluteLocation start, short previousLevel)
+	{
+		// Removing the light is somewhat more complicated as the operation needs to reverse if it finds a brighter than expected patch so this is 2 phases:
+		// -remove the light eminating from this source
+		// -re-flood the light from the other sources on the border
+		
+		// We need to have something to do.
+		Assert.assertTrue(previousLevel > 0);
+		Map<AbsoluteLocation, Short> updates = new HashMap<>();
+		Map<AbsoluteLocation, Short> reFlood = new HashMap<>();
+		updates.put(start, (short)0);
+		
+		Queue<_Step> queue = new LinkedList<>();
+		
+		// We assume that the starting block has already been cleared.
+		Assert.assertTrue(lookup.apply(start).getLight() == 0);
+		
+		// Enqueue the surrounding blocks.
+		_enqueueDarkNeighbours(queue, updates, reFlood, lookup, start, previousLevel);
+		
+		while (!queue.isEmpty())
+		{
+			_Step next = queue.remove();
+			
+			// We already checked this so add it to the updates and check its neighbours.
+			AbsoluteLocation location = next.location;
+			short light = next.lightValue;
+			// This is only in the queue if it could illuminate something else.
+			Assert.assertTrue(light > 0);
+			_enqueueDarkNeighbours(queue, updates, reFlood, lookup, location, light);
+		}
+		
+		// Remove the original since it was just part of a termination condition.
+		updates.remove(start);
+		
+		// Now, re-flood from all of those in the flood set, adding back new updates, over-writing whatever is there.
+		if (!reFlood.isEmpty())
+		{
+			// We need to make a shim over the lookup mechanism to return values from the previous phase updates.
+			Function<AbsoluteLocation, IBlockProxy> localLookup = (AbsoluteLocation location) -> {
+				IBlockProxy proxy = lookup.apply(location);
+				return (null != proxy)
+						? (updates.containsKey(location) ? new _ShimProxy(proxy, updates.get(location)) : proxy)
+						: null
+				;
+			};
+			
+			Map<AbsoluteLocation, Short> reUpdates = new HashMap<>();
+			Queue<_Step> reQueue = new LinkedList<>();
+			for (Map.Entry<AbsoluteLocation, Short> elt : reFlood.entrySet())
+			{
+				AbsoluteLocation location = elt.getKey();
+				short value = elt.getValue();
+				reUpdates.put(location, value);
+				_enqueueLightNeighbours(reQueue, reUpdates, localLookup, location, value);
+			}
+			_runLightQueue(reQueue, reUpdates, localLookup);
+			
+			// Remove the original since it was just part of a termination condition.
+			for (AbsoluteLocation location : reFlood.keySet())
+			{
+				reUpdates.remove(location);
+			}
+			
+			// Now, write these back over the updates.
+			updates.putAll(reUpdates);
+		}
+		
+		return updates;
+	}
+
+
+	private static void _runLightQueue(Queue<_Step> queue, Map<AbsoluteLocation, Short> updates, Function<AbsoluteLocation, IBlockProxy> lookup)
+	{
+		while (!queue.isEmpty())
+		{
+			_Step next = queue.remove();
+			
+			// We already checked this so add it to the updates and check its neighbours.
+			AbsoluteLocation location = next.location;
+			short light = next.lightValue;
+			// This is only in the queue if it could illuminate something else.
+			Assert.assertTrue(light > 1);
+			_enqueueLightNeighbours(queue, updates, lookup, location, light);
+		}
+	}
+
+	private static void _enqueueLightNeighbours(Queue<_Step> queue
+			, Map<AbsoluteLocation, Short> updates
+			, Function<AbsoluteLocation, IBlockProxy> lookup
+			, AbsoluteLocation start
+			, short light
+	)
+	{
+		_checkLightNeighbour(queue, updates, lookup, start.getRelative(0, 0, -1), light);
+		_checkLightNeighbour(queue, updates, lookup, start.getRelative(0, 0,  1), light);
+		_checkLightNeighbour(queue, updates, lookup, start.getRelative(0, -1, 0), light);
+		_checkLightNeighbour(queue, updates, lookup, start.getRelative(0,  1, 0), light);
+		_checkLightNeighbour(queue, updates, lookup, start.getRelative(-1, 0, 0), light);
+		_checkLightNeighbour(queue, updates, lookup, start.getRelative( 1, 0, 0), light);
+	}
+
+	private static void _checkLightNeighbour(Queue<_Step> queue
+			, Map<AbsoluteLocation, Short> updates
+			, Function<AbsoluteLocation, IBlockProxy> lookup
+			, AbsoluteLocation location
+			, short lightEntering
+	)
+	{
+		IBlockProxy proxy = lookup.apply(location);
+		if (null != proxy)
+		{
+			short previous = proxy.getLight();
+			// See if we have already processed this one (only happens for re-illumination or unusual opacity configurations).
+			if (updates.containsKey(location))
+			{
+				previous = updates.get(location);
+			}
+			short light = (short) (lightEntering - LightAspect.getOpacity(proxy.getBlock().asItem()));
+			if (light > previous)
+			{
+				// The block light can never be negative.
+				Assert.assertTrue(light > 0);
+				
+				// Add this to our map of updates and enqueue it for neighbour processing.
+				Assert.assertTrue(null == updates.put(location, light));
+				// See if the neighbours need to be lit.
+				if (light > 1)
+				{
+					queue.add(new _Step(location, light));
+				}
+			}
+		}
+	}
+
+	private static void _enqueueDarkNeighbours(Queue<_Step> queue
+			, Map<AbsoluteLocation, Short> updates
+			, Map<AbsoluteLocation, Short> reFlood
+			, Function<AbsoluteLocation, IBlockProxy> lookup
+			, AbsoluteLocation start
+			, short light
+	)
+	{
+		_checkDarkNeighbour(queue, updates, reFlood, lookup, start.getRelative(0, 0, -1), light);
+		_checkDarkNeighbour(queue, updates, reFlood, lookup, start.getRelative(0, 0,  1), light);
+		_checkDarkNeighbour(queue, updates, reFlood, lookup, start.getRelative(0, -1, 0), light);
+		_checkDarkNeighbour(queue, updates, reFlood, lookup, start.getRelative(0,  1, 0), light);
+		_checkDarkNeighbour(queue, updates, reFlood, lookup, start.getRelative(-1, 0, 0), light);
+		_checkDarkNeighbour(queue, updates, reFlood, lookup, start.getRelative( 1, 0, 0), light);
+	}
+
+	private static void _checkDarkNeighbour(Queue<_Step> queue
+			, Map<AbsoluteLocation, Short> updates
+			, Map<AbsoluteLocation, Short> reFlood
+			, Function<AbsoluteLocation, IBlockProxy> lookup
+			, AbsoluteLocation location
+			, short lightEntering
+	)
+	{
+		if (!updates.containsKey(location) && !reFlood.containsKey(location))
+		{
+			IBlockProxy proxy = lookup.apply(location);
+			if (null != proxy)
+			{
+				// Check the light level:
+				// -if it matches the expected value, set it to zero and enqueue it
+				// -if it is greater than the expected value, add it to the reflood set
+				// -cannot be less than expected
+				short light = (short) (lightEntering - LightAspect.getOpacity(proxy.getBlock().asItem()));
+				short existingLight = proxy.getLight();
+				if ((existingLight == light) && (existingLight > 0))
+				{
+					// Add this to our map of updates and enqueue it for neighbour processing.
+					Assert.assertTrue(null == updates.put(location, (short)0));
+					// See if the neighbours need to be lit.
+					if (light > 1)
+					{
+						queue.add(new _Step(location, light));
+					}
+				}
+				else if (existingLight > 1)
+				{
+					Assert.assertTrue(existingLight > light);
+					// We need to re-flood this one.
+					reFlood.put(location, existingLight);
+				}
+			}
+		}
+	}
+
+
+	private static record _Step(AbsoluteLocation location, short lightValue)
+	{}
+
+	private static class _ShimProxy implements IBlockProxy
+	{
+		private final IBlockProxy _proxy;
+		private final short _light;
+		
+		public _ShimProxy(IBlockProxy proxy, short light)
+		{
+			_proxy = proxy;
+			_light = light;
+		}
+		@Override
+		public Block getBlock()
+		{
+			return _proxy.getBlock();
+		}
+		@Override
+		public Inventory getInventory()
+		{
+			// Not expected in this class.
+			throw Assert.unreachable();
+		}
+		@Override
+		public short getDamage()
+		{
+			// Not expected in this class.
+			throw Assert.unreachable();
+		}
+		@Override
+		public CraftOperation getCrafting()
+		{
+			// Not expected in this class.
+			throw Assert.unreachable();
+		}
+		@Override
+		public FuelState getFuel()
+		{
+			// Not expected in this class.
+			throw Assert.unreachable();
+		}
+		@Override
+		public short getLight()
+		{
+			return _light;
+		}
+	}
+}
---
+++ october-common/src/test/java/com/jeffdisher/october/logic/TestLightBringer.java
@@ -0,0 +1,275 @@
+package com.jeffdisher.october.logic;
+
+import java.io.PrintStream;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jeffdisher.october.aspects.LightAspect;
+import com.jeffdisher.october.data.BlockProxy;
+import com.jeffdisher.october.data.CuboidData;
+import com.jeffdisher.october.data.MutableBlockProxy;
+import com.jeffdisher.october.registries.AspectRegistry;
+import com.jeffdisher.october.registries.ItemRegistry;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.BlockAddress;
+import com.jeffdisher.october.types.CuboidAddress;
+import com.jeffdisher.october.worldgen.CuboidGenerator;
+
+
+public class TestLightBringer
+{
+	@Test
+	public void singleLightSource()
+	{
+		// Create a single light source in the middle of an air cuboid and check how it propagates.
+		CuboidAddress address = new CuboidAddress((short)10, (short)10, (short)10);
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(address, ItemRegistry.AIR);
+		AbsoluteLocation centre = address.getBase().getRelative(16, 16, 16);
+		MutableBlockProxy source = new MutableBlockProxy(centre, cuboid);
+		source.setLight((short)15);
+		source.writeBack(cuboid);
+		
+		// We will show how this scales up with different light intensities (but not write back).
+		int[] expectedValues = new int[] {
+				0,
+				0,
+				6,
+				24,
+				62,
+				128,
+				230,
+				376,
+				574,
+				832,
+				1158,
+				1560,
+				2046,
+				2624,
+				3302,
+				4088,
+		};
+		for (short i = 2; i <= LightAspect.MAX_LIGHT; ++i)
+		{
+			source.setLight(i);
+			source.writeBack(cuboid);
+			Map<AbsoluteLocation, Short> updates = LightBringer.spreadLight((AbsoluteLocation location) -> {
+				return address.equals(location.getCuboidAddress())
+						? new BlockProxy(location.getBlockAddress(), cuboid)
+						: null
+				;
+			}, centre, i);
+			Assert.assertEquals(expectedValues[i], updates.size());
+		}
+	}
+
+	@Test
+	public void addRemoveTwoSource()
+	{
+		short value = LightAspect.MAX_LIGHT;
+		// Create a single light source in the middle of an air cuboid and check how it propagates.
+		CuboidAddress address = new CuboidAddress((short)10, (short)10, (short)10);
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(address, ItemRegistry.AIR);
+		AbsoluteLocation centre = address.getBase().getRelative(16, 16, 16);
+		cuboid.setData15(AspectRegistry.LIGHT, centre.getBlockAddress(), value);
+		
+		Map<AbsoluteLocation, Short> initialSet = LightBringer.spreadLight((AbsoluteLocation location) -> {
+			return address.equals(location.getCuboidAddress())
+					? new BlockProxy(location.getBlockAddress(), cuboid)
+					: null
+			;
+		}, centre, value);
+		Assert.assertEquals(4088, initialSet.size());
+		for (Map.Entry<AbsoluteLocation, Short> update : initialSet.entrySet())
+		{
+			cuboid.setData15(AspectRegistry.LIGHT, update.getKey().getBlockAddress(), update.getValue());
+		}
+		ByteBuffer buffer = ByteBuffer.allocate(40000);
+		Assert.assertNull(cuboid.serializeResumable(null, buffer));
+		
+		// Add a second light source near the first.
+		AbsoluteLocation secondLight = centre.getRelative(1, 1, 1);
+		_writeLight(System.out, cuboid, secondLight.getBlockAddress().z());
+		cuboid.setData15(AspectRegistry.LIGHT, secondLight.getBlockAddress(), value);
+		Map<AbsoluteLocation, Short> secondUpdates = LightBringer.spreadLight((AbsoluteLocation location) -> {
+			return address.equals(location.getCuboidAddress())
+					? new BlockProxy(location.getBlockAddress(), cuboid)
+					: null
+			;
+		}, secondLight, value);
+		Assert.assertEquals(2359, secondUpdates.size());
+		for (Map.Entry<AbsoluteLocation, Short> update : secondUpdates.entrySet())
+		{
+			cuboid.setData15(AspectRegistry.LIGHT, update.getKey().getBlockAddress(), update.getValue());
+		}
+		_writeLight(System.out, cuboid, secondLight.getBlockAddress().z());
+		
+		// Now, remove the light source and watch the updates.
+		cuboid.setData15(AspectRegistry.LIGHT, secondLight.getBlockAddress(), (short)0);
+		Map<AbsoluteLocation, Short> resetUpdates = LightBringer.removeLight((AbsoluteLocation location) -> {
+			return address.equals(location.getCuboidAddress())
+					? new BlockProxy(location.getBlockAddress(), cuboid)
+					: null
+			;
+		}, centre.getRelative(1, 1, 1), value);
+		Assert.assertEquals(2360, resetUpdates.size());
+		for (Map.Entry<AbsoluteLocation, Short> update : resetUpdates.entrySet())
+		{
+			cuboid.setData15(AspectRegistry.LIGHT, update.getKey().getBlockAddress(), update.getValue());
+		}
+		_writeLight(System.out, cuboid, secondLight.getBlockAddress().z());
+		
+		ByteBuffer buffer2 = ByteBuffer.allocate(40000);
+		Assert.assertNull(cuboid.serializeResumable(null, buffer2));
+		Assert.assertArrayEquals(buffer.array(), buffer2.array());
+		
+		// We expect that the reset will include 1 additional update since it re-lights the second block source.
+		int missingCount = 0;
+		for (AbsoluteLocation loc : resetUpdates.keySet())
+		{
+			if (!secondUpdates.containsKey(loc))
+			{
+				// This should only happen for the second light location, since it is re-lit by the first light.
+				missingCount += 1;
+				Assert.assertEquals(secondLight, loc);
+			}
+		}
+		Assert.assertEquals(1, missingCount);
+	}
+
+	@Test
+	public void maze()
+	{
+		// Add and them remove a single light source in a 2D maze, in a block of stone.
+		short value = LightAspect.MAX_LIGHT;
+		CuboidAddress address = new CuboidAddress((short)10, (short)10, (short)10);
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(address, ItemRegistry.STONE);
+		BlockAddress source = _loadBlockMap(cuboid, (byte)16, ""
+				+ "SSSSSSSSSSSSSSS SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSS SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSS WWWSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSS SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSS   WW SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSS SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSS W SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSS S W   SSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSS S SSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSS SXSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSS  W W  SSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSS SSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSS  SSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSS  SSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+				+ "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+		);
+		ByteBuffer buffer = ByteBuffer.allocate(40000);
+		Assert.assertNull(cuboid.serializeResumable(null, buffer));
+		
+		cuboid.setData15(AspectRegistry.LIGHT, source, value);
+		AbsoluteLocation target = address.getBase().getRelative(source.x(), source.y(), source.z());
+		
+		Map<AbsoluteLocation, Short> updates = LightBringer.spreadLight((AbsoluteLocation location) -> {
+			return address.equals(location.getCuboidAddress())
+					? new BlockProxy(location.getBlockAddress(), cuboid)
+					: null
+			;
+		}, target, value);
+		// We expect 38 updates to fill the maze.
+		int expectedUpdates = 38;
+		Assert.assertEquals(expectedUpdates, updates.size());
+		for (Map.Entry<AbsoluteLocation, Short> update : updates.entrySet())
+		{
+			cuboid.setData15(AspectRegistry.LIGHT, update.getKey().getBlockAddress(), update.getValue());
+		}
+		_writeLight(System.out, cuboid, source.z());
+		
+		cuboid.setData15(AspectRegistry.LIGHT, source, (short)0);
+		updates = LightBringer.removeLight((AbsoluteLocation location) -> {
+			return address.equals(location.getCuboidAddress())
+					? new BlockProxy(location.getBlockAddress(), cuboid)
+					: null
+			;
+		}, target, value);
+		// The same number to clear the maze.
+		Assert.assertEquals(expectedUpdates, updates.size());
+		for (Map.Entry<AbsoluteLocation, Short> update : updates.entrySet())
+		{
+			cuboid.setData15(AspectRegistry.LIGHT, update.getKey().getBlockAddress(), update.getValue());
+		}
+		
+		ByteBuffer buffer2 = ByteBuffer.allocate(40000);
+		Assert.assertNull(cuboid.serializeResumable(null, buffer2));
+		Assert.assertArrayEquals(buffer.array(), buffer2.array());
+	}
+
+
+	private static void _writeLight(PrintStream stream, CuboidData cuboid, byte z)
+	{
+		stream.println("--------------------------------");
+		for (byte y = 31; y >= 0; --y)
+		{
+			for (byte x = 0; x < 32; ++x)
+			{
+				BlockAddress address = new BlockAddress(x, y, z);
+				short value = cuboid.getData15(AspectRegistry.LIGHT, address);
+				stream.print(Integer.toHexString(value));
+			}
+			stream.println();
+		}
+	}
+
+	private static BlockAddress _loadBlockMap(CuboidData cuboid, byte z, String map)
+	{
+		BlockAddress match = null;
+		int index = 0;
+		for (byte y = 31; y >= 0; --y)
+		{
+			for (byte x = 0; x < 32; ++x)
+			{
+				char c = map.charAt(index);
+				index += 1;
+				short value;
+				switch(c)
+				{
+				case ' ':
+					value = ItemRegistry.AIR.number();
+					break;
+				case 'S':
+					value = ItemRegistry.STONE.number();
+					break;
+				case 'W':
+					value = ItemRegistry.WATER_SOURCE.number();
+					break;
+				case 'X':
+					value = ItemRegistry.AIR.number();
+					Assert.assertNull(match);
+					match = new BlockAddress(x, y, z);
+					break;
+					default:
+						throw new AssertionError("Unknown");
+				}
+				BlockAddress address = new BlockAddress(x, y, z);
+				cuboid.setData15(AspectRegistry.BLOCK, address, value);
+			}
+		}
+		return match;
+	}
+}
