b2fe61c339f589c9c7728f082ca7286fc82a960d
Jeff Disher <jeff@jeffdisher.com>
2024-04-10 22:15:48 -0400

    The FlatWorldGenerator now injects a structure into the origin cuboids
    -this avoids the need to drop starting items on the ground and also produces a well-lit starting place

--- october-server/src/main/java/com/jeffdisher/october/persistence/FlatWorldGenerator.java
+++ october-server/src/main/java/com/jeffdisher/october/persistence/FlatWorldGenerator.java
@@ -5,13 +5,16 @@ import java.util.function.Function;
 
 import com.jeffdisher.october.aspects.AspectRegistry;
 import com.jeffdisher.october.aspects.Environment;
-import com.jeffdisher.october.aspects.InventoryAspect;
 import com.jeffdisher.october.data.CuboidData;
+import com.jeffdisher.october.logic.ScheduledMutation;
+import com.jeffdisher.october.mutations.IMutationBlock;
+import com.jeffdisher.october.types.AbsoluteLocation;
 import com.jeffdisher.october.types.Block;
 import com.jeffdisher.october.types.BlockAddress;
 import com.jeffdisher.october.types.CuboidAddress;
-import com.jeffdisher.october.types.Inventory;
 import com.jeffdisher.october.worldgen.CuboidGenerator;
+import com.jeffdisher.october.worldgen.Structure;
+import com.jeffdisher.october.worldgen.StructureLoader;
 
 
 /**
@@ -20,6 +23,28 @@ import com.jeffdisher.october.worldgen.CuboidGenerator;
  */
 public class FlatWorldGenerator implements Function<CuboidAddress, SuspendedCuboid<CuboidData>>
 {
+	// We want to generate a basic starting structure around the world centre.  For now, we just build that from a static string.
+	public static final String[] STRUCTURE = new String[] {""
+			+ "DDDDDDDD\n"
+			+ "DBBBBBBD\n"
+			+ "DBWBBWBD\n"
+			+ "DBBBBBBD\n"
+			+ "DBBBBBBD\n"
+			+ "DBWBBWBD\n"
+			+ "DBBBBBBD\n"
+			+ "DDDDDDDD\n"
+			, ""
+			+ "S PPPP S\n"
+			+ " L    L \n"
+			+ "P      P\n"
+			+ "P      P\n"
+			+ "P      P\n"
+			+ "P      P\n"
+			+ " L    L \n"
+			+ "S PPPP S\n"
+	};
+	public static final AbsoluteLocation BASE = new AbsoluteLocation(-4, -4, -1);
+
 	@Override
 	public SuspendedCuboid<CuboidData> apply(CuboidAddress address)
 	{
@@ -40,18 +65,32 @@ public class FlatWorldGenerator implements Function<CuboidAddress, SuspendedCubo
 		else
 		{
 			data = CuboidGenerator.createFilledCuboid(address, env.blocks.AIR);
-			// If this is the 0,0,0 cuboid, drop other useful testing items on the ground.
-			if ((0 == address.x()) && (0 == address.y()) && (0 == address.z()))
-			{
-				Inventory starting = Inventory.start(InventoryAspect.CAPACITY_BLOCK_EMPTY)
-						.add(env.items.SAPLING, 2)
-						.add(env.items.LANTERN, 2)
-						.add(env.items.WHEAT_SEED, 2)
-						.finish();
-				data.setDataSpecial(AspectRegistry.INVENTORY, new BlockAddress((byte)0, (byte)0, (byte)0), starting);
-			}
 		}
-		return new SuspendedCuboid<>(data, List.of());
+		// See if this is a cuboid where we want to generate our structure (it is in the 8 cuboids around the origin).
+		List<ScheduledMutation> mutations;
+		if (((-1 == address.x()) || (0 == address.x()))
+				&& ((-1 == address.y()) || (0 == address.y()))
+				&& ((-1 == address.z()) || (0 == address.z()))
+		)
+		{
+			StructureLoader loader = new StructureLoader(env.blocks);
+			Structure structure = loader.loadFromStrings(STRUCTURE);
+			AbsoluteLocation baseOffset = new AbsoluteLocation(
+					(0 == address.x()) ? BASE.x() : (32 + BASE.x()),
+					(0 == address.y()) ? BASE.y() : (32 + BASE.y()),
+					(0 == address.z()) ? BASE.z() : (32 + BASE.z())
+			);
+			List<IMutationBlock> immediateMutations = structure.applyToCuboid(data, baseOffset);
+			mutations = immediateMutations.stream()
+					.map((IMutationBlock mutation) -> new ScheduledMutation(mutation, 0L))
+					.toList()
+			;
+		}
+		else
+		{
+			mutations = List.of();
+		}
+		return new SuspendedCuboid<CuboidData>(data, mutations);
 	}
 
 	private static void _fillPlane(CuboidData data, byte z, Block block)
