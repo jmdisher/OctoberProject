ffba3fde1a8466572bbf137dfdc4d0fc3316aeac
Jeff Disher <jeff@jeffdisher.com>
2024-09-12 15:42:53 -0400

    Adjusted AutoWalkingClient to allow break/place blocks
    -these clients now wait for commands from the server before starting (so that the server can position them and set creative mode - required for most modes)

--- october-integration/src/main/java/com/jeffdisher/october/integration/AutoWalkingClient.java
+++ october-integration/src/main/java/com/jeffdisher/october/integration/AutoWalkingClient.java
@@ -7,21 +7,40 @@ import com.jeffdisher.october.aspects.Environment;
 import com.jeffdisher.october.data.ColumnHeightMap;
 import com.jeffdisher.october.data.IReadOnlyCuboidData;
 import com.jeffdisher.october.mutations.EntityChangeMove;
+import com.jeffdisher.october.mutations.MutationEntitySelectItem;
+import com.jeffdisher.october.mutations.MutationPlaceSelectedBlock;
 import com.jeffdisher.october.process.ClientProcess;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.Block;
 import com.jeffdisher.october.types.CuboidAddress;
 import com.jeffdisher.october.types.Entity;
+import com.jeffdisher.october.types.EntityConstants;
+import com.jeffdisher.october.types.EntityLocation;
+import com.jeffdisher.october.types.EntityType;
+import com.jeffdisher.october.types.EntityVolume;
 import com.jeffdisher.october.types.PartialEntity;
+import com.jeffdisher.october.utils.Assert;
 
 
 /**
- * A testing program which will join a world and walk in a single cardinal direction forever.  This is intended as a
- * network stress test as it is easy to instantiate many of these into the same world.
- * It takes 4 argumens:
+ * A testing program which joins a world and will then wait for a message from the server to tell it what to do.  This
+ * command takes the form "ACTION DIRECTION":
+ * -ACTION is one of "WALK", "BREAK", or "PLACE".
+ * -DIRECTION is one of the cardinal directions
+ * Once receiving this command, the client will begin walking in that direction and potentially breaking or placing
+ * blocks behind it, as it moves.
+ * Note that it assumes that it has been put into creative mode if breaking or placing, since it needs to immediately
+ * break a block and have an infinite amount of stone brick blocks, if placing.
+ * 
+ * Starting the program takes 3 arguments:
  * -hostname
  * -port
  * -client name
- * -direction
+ * 
  * The program will exit when the server disconnects.
+ * 
+ * The point of this is to act as a stress test as multiple instances of this client can be connected and they will all
+ * move and potentially interact with the world as they go, allowing insight into server scalability.
  */
 public class AutoWalkingClient
 {
@@ -32,43 +51,125 @@ public class AutoWalkingClient
 
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
-		if (4 == args.length)
+		if (3 == args.length)
 		{
 			String hostname = args[0];
 			int port = Integer.parseInt(args[1]);
 			String clientName = args[2];
-			EntityChangeMove.Direction direction = EntityChangeMove.Direction.valueOf(args[3]);
 			InetSocketAddress serverAddress = new InetSocketAddress(hostname, port);
-			_runClient(serverAddress, clientName, direction);
+			
+			_Packaged packaged = _connectAndWaitForCommand(serverAddress, clientName);
+			if (null != packaged)
+			{
+				_runClient(packaged.listener, packaged.client, packaged.command);
+			}
 		}
 		else
 		{
-			System.out.println("Usage:  <hostname> <port> <client_name> <direction>");
+			System.out.println("Usage:  <hostname> <port> <client_name>");
 			System.exit(1);
 		}
 	}
 
 
-	private static void _runClient(InetSocketAddress serverAddress, String clientName, EntityChangeMove.Direction direction) throws IOException, InterruptedException
+	private static _Packaged _connectAndWaitForCommand(InetSocketAddress serverAddress, String clientName) throws IOException, InterruptedException
 	{
 		Environment.createSharedInstance();
 		_Listener listener = new _Listener();
 		ClientProcess client = new ClientProcess(listener, serverAddress.getAddress(), serverAddress.getPort(), clientName);
 		client.waitForLocalEntity(System.currentTimeMillis());
 		
+		// Wait until we get a command.
+		System.out.println("Connected to server as " + listener.thisAssignedId + ".  Remember to set creative mode: \"!set_mode " + listener.thisAssignedId + " CREATIVE\"");
+		System.out.println("Waiting for server command of the flavour: \"!message " + listener.thisAssignedId + " ACTION DIRECTION\"...");
+		_Command command = null;
+		while ((null == command) && listener.isConnected)
+		{
+			Thread.sleep(100L);
+			client.runPendingCalls(System.currentTimeMillis());
+			if (null != listener.lastCommandFromServer)
+			{
+				command = _parseCommand(listener.lastCommandFromServer);
+			}
+		}
+		return (null != command)
+				? new _Packaged(listener, client, command)
+				: null
+		;
+	}
+
+	private static void _runClient(_Listener listener, ClientProcess client, _Command command) throws InterruptedException
+	{
+		// We can now create the environment.
+		Environment env = Environment.getShared();
+		
+		// Select the block (note that this assumes that we are in creative mode).
+		Block stoneBrick = env.blocks.getAsPlaceableBlock(env.items.getItemById("op.stone_brick"));
+		MutationEntitySelectItem select = new MutationEntitySelectItem(stoneBrick.item().number());
+		client.sendAction(select, System.currentTimeMillis());
+		
+		// We can now run the loop.
+		System.out.println("Running command " + command.action + " " + command.direction);
 		long lastReport = 0L;
+		AbsoluteLocation lastBlock = listener.thisEntity.location().getBlockLocation();
 		while (listener.isConnected)
 		{
 			Thread.sleep(100L);
 			long currentTimeMillis = System.currentTimeMillis();
-			client.moveHorizontalFully(direction, currentTimeMillis);
 			if (currentTimeMillis >= (lastReport + REPORT_INTERVAL_MILLIS))
 			{
 				System.out.println("Location: " + listener.thisEntity.location());
 				lastReport = currentTimeMillis;
 			}
+			if (_isOutsideOfBlock(lastBlock, listener.thisEntity))
+			{
+				// Take special action.
+				switch (command.action)
+				{
+				case WALK:
+					// Do nothing special.
+					break;
+				case BREAK:
+					client.hitBlock(lastBlock.getRelative(0, 0, -1), currentTimeMillis);
+					break;
+				case PLACE:
+					client.sendAction(new MutationPlaceSelectedBlock(lastBlock, lastBlock), currentTimeMillis);
+					break;
+					default:
+						throw Assert.unreachable();
+				}
+				lastBlock = listener.thisEntity.location().getBlockLocation();
+			}
+			else
+			{
+				client.moveHorizontalFully(command.direction, currentTimeMillis);
+			}
 		}
 		client.disconnect();
+	}
+
+	private static _Command _parseCommand(String commandText)
+	{
+		String[] parts = commandText.split(" ");
+		// We only expect the 2 strings: "ACTION DIRECTION"
+		Assert.assertTrue(2 == parts.length);
+		_Action action = _Action.valueOf(parts[0]);
+		Assert.assertTrue(null != action);
+		EntityChangeMove.Direction direction = EntityChangeMove.Direction.valueOf(parts[1]);
+		Assert.assertTrue(null != direction);
+		return new _Command(action, direction);
+	}
+
+	private static boolean _isOutsideOfBlock(AbsoluteLocation block, Entity entity)
+	{
+		EntityLocation base = entity.location();
+		EntityVolume volume = EntityConstants.getVolume(EntityType.PLAYER);
+		EntityLocation edge = new EntityLocation(base.x() + volume.width(), base.y() + volume.width(), base.z() + volume.height());
+		
+		// We will say that we are out of this block if neither the base nor the edge are within this block (note that this could fail if the volume is larger than 1 block in any direction).
+		AbsoluteLocation baseBlock = base.getBlockLocation();
+		AbsoluteLocation edgeBlock = edge.getBlockLocation();
+		return !block.equals(baseBlock) && !block.equals(edgeBlock);
 	}
 
 
@@ -77,6 +178,7 @@ public class AutoWalkingClient
 		public int thisAssignedId;
 		public boolean isConnected;
 		public Entity thisEntity;
+		public String lastCommandFromServer;
 		
 		@Override
 		public void connectionEstablished(int assignedEntityId)
@@ -149,6 +251,26 @@ public class AutoWalkingClient
 			{
 				System.out.println("Chat from " + senderId + ": " + message);
 			}
+			if (0 == senderId)
+			{
+				this.lastCommandFromServer = message;
+			}
 		}
 	}
+
+	private static enum _Action
+	{
+		WALK,
+		BREAK,
+		PLACE,
+	}
+
+	private static record _Command(_Action action
+			, EntityChangeMove.Direction direction
+	) {}
+
+	private static record _Packaged(_Listener listener
+			, ClientProcess client
+			, _Command command
+	) {}
 }
