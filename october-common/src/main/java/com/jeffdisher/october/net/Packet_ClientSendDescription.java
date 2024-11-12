2700f23add8159561a68f79e96d9cfdc7aa6aeeb
Jeff Disher <jeff@jeffdisher.com>
2024-11-12 18:01:32 -0500

    Updated network protocol version to 3
    -this change is being made preemptively before the next release since storage and mutation type changes are planned before the next release

--- october-common/src/main/java/com/jeffdisher/october/net/Packet_ClientSendDescription.java
+++ october-common/src/main/java/com/jeffdisher/october/net/Packet_ClientSendDescription.java
@@ -17,8 +17,9 @@ public class Packet_ClientSendDescription extends PacketFromClient
 	/**
 	 * Protocol version 0 was used in v1.0-pre4 and earlier.
 	 * Protocol version 1 was used in v1.0-pre6 and earlier.
+	 * Protocol version 2 was used in v1.0.1 and earlier.
 	 */
-	public static final int NETWORK_PROTOCOL_VERSION = 2;
+	public static final int NETWORK_PROTOCOL_VERSION = 3;
 
 	public static void register(Function<ByteBuffer, Packet>[] opcodeTable)
 	{
