c5802e8f8b9cd3453edd06f7acac4fc9bce4920c
Jeff Disher <jeff@jeffdisher.com>
2025-06-02 23:47:18 -0400

    Added a new utility to run a single entity mutation and follow-ups
    -this is not currently used but will eventually be used on the client to apply incomplete entity changes to the local state, on top of the SpeculativeProjection (since it only applies complete changes)
    -the details of how follow-ups are handled may evolve but ideally there will be some opportunity to apply a common pattern for any changes to this and the SpeculativeProjection

---
+++ october-client/src/main/java/com/jeffdisher/october/client/OneOffRunner.java
@@ -0,0 +1,172 @@
+package com.jeffdisher.october.client;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.jeffdisher.october.data.BlockProxy;
+import com.jeffdisher.october.data.CuboidHeightMap;
+import com.jeffdisher.october.data.IReadOnlyCuboidData;
+import com.jeffdisher.october.logic.CommonChangeSink;
+import com.jeffdisher.october.logic.CommonMutationSink;
+import com.jeffdisher.october.logic.CrowdProcessor;
+import com.jeffdisher.october.logic.ProcessorElement;
+import com.jeffdisher.october.logic.ScheduledChange;
+import com.jeffdisher.october.logic.ScheduledMutation;
+import com.jeffdisher.october.logic.SyncPoint;
+import com.jeffdisher.october.logic.WorldProcessor;
+import com.jeffdisher.october.mutations.IMutationEntity;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.CuboidAddress;
+import com.jeffdisher.october.types.Entity;
+import com.jeffdisher.october.types.IMutablePlayerEntity;
+import com.jeffdisher.october.types.LazyLocationCache;
+import com.jeffdisher.october.types.MinimalEntity;
+import com.jeffdisher.october.types.PartialEntity;
+import com.jeffdisher.october.types.TickProcessingContext;
+import com.jeffdisher.october.types.WorldConfig;
+
+
+/**
+ * A helper class which exposes a helper just to run a single entity change and one step of follow-up block mutations.
+ */
+public class OneOffRunner
+{
+	/**
+	 * Runs a single entity change on the given description of state, returning the updated version of the state (null
+	 * if the entity change failed).
+	 * Note that a single iteration of follow-up block change mutations will also be run.
+	 * 
+	 * @param state The state before the change.
+	 * @param eventSink The sink where events will be delivered.
+	 * @param millisPerTick How many milliseconds are in a tick.
+	 * @param currentTickTimeMillis The current time, in milliseconds.
+	 * @param mutation The entity change to run.
+	 * @return The state after the change, or null if the entity change failed.
+	 */
+	public static StatePackage runOneChange(StatePackage state
+			, TickProcessingContext.IEventSink eventSink
+			, long millisPerTick
+			, long currentTickTimeMillis 
+			, IMutationEntity<IMutablePlayerEntity> mutation
+	)
+	{
+		// Setup components.
+		ProcessorElement singleThreadElement = new ProcessorElement(0, new SyncPoint(1), new AtomicInteger(0));
+		CommonMutationSink newMutationSink = new CommonMutationSink();
+		CommonChangeSink newChangeSink = new CommonChangeSink();
+		TickProcessingContext context = _createContext(state, newMutationSink, newChangeSink, eventSink, millisPerTick, currentTickTimeMillis);
+		
+		// Run initial change.
+		int thisEntityId = state.thisEntity().id();
+		ScheduledChange scheduled = new ScheduledChange(mutation, 0L);
+		CrowdProcessor.ProcessedGroup innerGroup = CrowdProcessor.processCrowdGroupParallel(singleThreadElement
+				, Map.of(thisEntityId, state.thisEntity())
+				, context
+				, Map.of(thisEntityId, List.of(scheduled))
+		);
+		Entity updatedEntity = (innerGroup.committedMutationCount() > 0)
+				? innerGroup.updatedEntities().get(thisEntityId)
+				: null
+		;
+		List<ScheduledMutation> immediateMutations = newMutationSink.takeExportedMutations().stream()
+				.filter((ScheduledMutation input) -> (0L == input.millisUntilReady()))
+				.toList()
+		;
+		
+		// Run follow-ups against blocks.
+		Map<CuboidAddress, IReadOnlyCuboidData> changedCuboids = Map.of();
+		Map<CuboidAddress, CuboidHeightMap> heightFragment = Map.of();
+		if ((null != updatedEntity) && !immediateMutations.isEmpty())
+		{
+			Map<CuboidAddress, List<ScheduledMutation>> splitMutations = new HashMap<>();
+			for (ScheduledMutation imm : immediateMutations)
+			{
+				CuboidAddress address = imm.mutation().getAbsoluteLocation().getCuboidAddress();
+				if (!splitMutations.containsKey(address))
+				{
+					splitMutations.put(address, new ArrayList<>());
+				}
+				splitMutations.get(address).add(imm);
+			}
+			WorldProcessor.ProcessedFragment innerFragment = WorldProcessor.processWorldFragmentParallel(singleThreadElement
+					, state.world
+					, state.heights
+					, context
+					, splitMutations
+					, Map.of()
+					, Map.of()
+					, Map.of()
+					, Map.of()
+					, Set.of()
+			);
+			changedCuboids = innerFragment.stateFragment();
+			heightFragment = innerFragment.heightFragment();
+		}
+		
+		// Repackage results.
+		StatePackage updatedState = null;
+		if (null != updatedEntity)
+		{
+			Map<CuboidAddress, IReadOnlyCuboidData> initialCuboids = new HashMap<>(state.world);
+			Map<CuboidAddress, CuboidHeightMap> initialHeights = new HashMap<>(state.heights);
+			Map<Integer, PartialEntity> initialCrowd = new HashMap<>(state.otherEntities);
+			initialCuboids.putAll(changedCuboids);
+			initialHeights.putAll(heightFragment);
+			updatedState = new StatePackage(updatedEntity, initialCuboids, initialHeights, initialCrowd);
+		}
+		return updatedState;
+	}
+
+
+	private static TickProcessingContext _createContext(StatePackage state
+			, CommonMutationSink newMutationSink
+			, CommonChangeSink newChangeSink
+			, TickProcessingContext.IEventSink eventSink
+			, long millisPerTick
+			, long currentTickTimeMillis 
+	)
+	{
+		long gameTick = 0L;
+		LazyLocationCache<BlockProxy> cachingLoader = new LazyLocationCache<>((AbsoluteLocation location) -> {
+			IReadOnlyCuboidData cuboid = state.world.get(location.getCuboidAddress());
+			return (null != cuboid)
+				? new BlockProxy(location.getBlockAddress(), cuboid)
+				: null
+			;
+		});
+		int thisEntityId = state.thisEntity().id();
+		TickProcessingContext context = new TickProcessingContext(gameTick
+				, cachingLoader
+				, (Integer entityId) -> (thisEntityId == entityId)
+					? MinimalEntity.fromEntity(state.thisEntity())
+					: MinimalEntity.fromPartialEntity(state.otherEntities.get(entityId))
+				, null
+				, newMutationSink
+				, newChangeSink
+				// We never spawn creatures on the client so no ID assigner.
+				, null
+				// We need a random number generator for a few cases (like attack) but the server will send us the authoritative result.
+				, (int bound) -> 0
+				, eventSink
+				// By default, we run in hostile mode.
+				, new WorldConfig()
+				, millisPerTick
+				, currentTickTimeMillis
+		);
+		return context;
+	}
+
+
+	/**
+	 * A packaged read-only state.
+	 */
+	public static record StatePackage(Entity thisEntity
+		, Map<CuboidAddress, IReadOnlyCuboidData> world
+		, Map<CuboidAddress, CuboidHeightMap> heights
+		, Map<Integer, PartialEntity> otherEntities
+	) {}
+}
---
+++ october-client/src/test/java/com/jeffdisher/october/client/TestOneOffRunner.java
@@ -0,0 +1,112 @@
+package com.jeffdisher.october.client;
+
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jeffdisher.october.aspects.AspectRegistry;
+import com.jeffdisher.october.aspects.Environment;
+import com.jeffdisher.october.data.CuboidData;
+import com.jeffdisher.october.logic.HeightMapHelpers;
+import com.jeffdisher.october.mutations.MutationPlaceSelectedBlock;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.CuboidAddress;
+import com.jeffdisher.october.types.Entity;
+import com.jeffdisher.october.types.EntityLocation;
+import com.jeffdisher.october.types.EventRecord;
+import com.jeffdisher.october.types.Item;
+import com.jeffdisher.october.types.MutableEntity;
+import com.jeffdisher.october.types.TickProcessingContext;
+import com.jeffdisher.october.utils.CuboidGenerator;
+
+
+public class TestOneOffRunner
+{
+	private static final long MILLIS_PER_TICK = 100L;
+	private static Environment ENV;
+	private static Item STONE_ITEM;
+	@BeforeClass
+	public static void setup()
+	{
+		ENV = Environment.createSharedInstance();
+		STONE_ITEM = ENV.items.getItemById("op.stone");
+	}
+	@AfterClass
+	public static void tearDown()
+	{
+		Environment.clearSharedInstance();
+	}
+
+	@Test
+	public void basicMutation() throws Throwable
+	{
+		// Create a world with the player and 2 cuboids, showing that we can place a block and verify changes.
+		MutableEntity mutable = MutableEntity.createForTest(1);
+		mutable.newLocation = new EntityLocation(5.0f, 5.0f, 0.0f);
+		mutable.setSelectedKey(1);
+		mutable.newInventory.addAllItems(STONE_ITEM, 1);
+		Entity entity = mutable.freeze();
+		AbsoluteLocation target = mutable.newLocation.getBlockLocation().getRelative(1, 1, 0);
+		
+		CuboidAddress airAddress = CuboidAddress.fromInt(0, 0, 0);
+		CuboidData airCuboid = CuboidGenerator.createFilledCuboid(airAddress, ENV.special.AIR);
+		CuboidAddress stoneAddress = CuboidAddress.fromInt(0, 0, -1);
+		CuboidData stoneCuboid = CuboidGenerator.createFilledCuboid(stoneAddress, ENV.blocks.fromItem(STONE_ITEM));
+		MutationPlaceSelectedBlock place = new MutationPlaceSelectedBlock(target, null);
+		
+		OneOffRunner.StatePackage start = new OneOffRunner.StatePackage(entity, Map.of(airAddress, airCuboid
+				, stoneAddress, stoneCuboid
+		), Map.of(airAddress, HeightMapHelpers.buildHeightMap(airCuboid)
+				, stoneAddress, HeightMapHelpers.buildHeightMap(stoneCuboid)
+		), Map.of());
+		_Events catcher = new _Events();
+		OneOffRunner.StatePackage end = OneOffRunner.runOneChange(start, catcher, MILLIS_PER_TICK, 1L, place);
+		
+		Assert.assertEquals(EventRecord.Type.BLOCK_PLACED, catcher.event.type());
+		Assert.assertTrue(start.world().get(stoneAddress) == end.world().get(stoneAddress));
+		Assert.assertEquals(STONE_ITEM.number(), end.world().get(stoneAddress).getData15(AspectRegistry.BLOCK, target.getBlockAddress()));
+		Assert.assertEquals(0, end.thisEntity().inventory().currentEncumbrance);
+	}
+
+	@Test
+	public void failedBasicMutation() throws Throwable
+	{
+		MutableEntity mutable = MutableEntity.createForTest(1);
+		mutable.newLocation = new EntityLocation(5.0f, 5.0f, 0.0f);
+		mutable.setSelectedKey(1);
+		mutable.newInventory.addAllItems(STONE_ITEM, 1);
+		Entity entity = mutable.freeze();
+		AbsoluteLocation target = mutable.newLocation.getBlockLocation().getRelative(1, 1, 0);
+		
+		CuboidAddress airAddress = CuboidAddress.fromInt(0, 0, 0);
+		CuboidData airCuboid = CuboidGenerator.createFilledCuboid(airAddress, ENV.special.AIR);
+		airCuboid.setData15(AspectRegistry.BLOCK, target.getBlockAddress(), STONE_ITEM.number());
+		CuboidAddress stoneAddress = CuboidAddress.fromInt(0, 0, -1);
+		CuboidData stoneCuboid = CuboidGenerator.createFilledCuboid(stoneAddress, ENV.blocks.fromItem(STONE_ITEM));
+		MutationPlaceSelectedBlock place = new MutationPlaceSelectedBlock(target, null);
+		
+		OneOffRunner.StatePackage start = new OneOffRunner.StatePackage(entity, Map.of(airAddress, airCuboid
+				, stoneAddress, stoneCuboid
+		), Map.of(airAddress, HeightMapHelpers.buildHeightMap(airCuboid)
+				, stoneAddress, HeightMapHelpers.buildHeightMap(stoneCuboid)
+		), Map.of());
+		_Events catcher = new _Events();
+		OneOffRunner.StatePackage end = OneOffRunner.runOneChange(start, catcher, MILLIS_PER_TICK, 1L, place);
+		Assert.assertNull(end);
+	}
+
+
+	private static class _Events implements TickProcessingContext.IEventSink
+	{
+		private EventRecord event;
+		@Override
+		public void post(EventRecord event)
+		{
+			Assert.assertNull(this.event);
+			this.event = event;
+		}
+	}
+}
