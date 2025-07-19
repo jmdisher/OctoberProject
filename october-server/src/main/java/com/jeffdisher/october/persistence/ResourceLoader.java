3be16d3dd0c24e874e14ece63f7de1635b5672e0
Jeff Disher <jeff@jeffdisher.com>
2025-07-19 19:51:01 -0400

    Update network to version 10 and storage to version 8
    -the next version has plans to change on-disk and network data shapes so this version change makes sense now
    -no changes have been made yet

--- october-server/src/main/java/com/jeffdisher/october/persistence/ResourceLoader.java
+++ october-server/src/main/java/com/jeffdisher/october/persistence/ResourceLoader.java
@@ -70,6 +70,7 @@ public class ResourceLoader
 	 * Version 4 was used in v1.3 and earlier, and is supported (5 only adds and deprecates data).
 	 * Version 5 was used in v1.5 and earlier, and is supported.
 	 * Version 6 was used in v1.6 and earlier, and is supported (7 only adds data).
+	 * Version 7 was used in v1.7 and earlier, and is supported.
 	 */
 	public static final int VERSION_CUBOID_V1 = 1;
 	public static final int VERSION_CUBOID_V2 = 2;
@@ -77,7 +78,8 @@ public class ResourceLoader
 	public static final int VERSION_CUBOID_V4 = 4;
 	public static final int VERSION_CUBOID_V5 = 5;
 	public static final int VERSION_CUBOID_V6 = 6;
-	public static final int VERSION_CUBOID = 7;
+	public static final int VERSION_CUBOID_V7 = 7;
+	public static final int VERSION_CUBOID = 8;
 	/**
 	 * Version 0 was used in v1.0-pre6 and earlier, no longer supported (pre-releases have no migration support).
 	 * Version 1 was used in v1.0.1 and earlier, and is supported.
@@ -86,6 +88,7 @@ public class ResourceLoader
 	 * Version 4 was used in v1.3 and earlier, and is supported (5 only adds and deprecates data).
 	 * Version 5 was used in v1.5 and earlier, and is supported (6 only adds and deprecates data).
 	 * Version 6 was used in v1.6 and earlier, and is supported (7 only adds data).
+	 * Version 7 was used in v1.7 and earlier, and is supported.
 	 */
 	public static final int VERSION_ENTITY_V1 = 1;
 	public static final int VERSION_ENTITY_V2 = 2;
@@ -93,7 +96,8 @@ public class ResourceLoader
 	public static final int VERSION_ENTITY_V4 = 4;
 	public static final int VERSION_ENTITY_V5 = 5;
 	public static final int VERSION_ENTITY_V6 = 6;
-	public static final int VERSION_ENTITY = 7;
+	public static final int VERSION_ENTITY_V7 = 7;
+	public static final int VERSION_ENTITY = 8;
 	public static final int SERIALIZATION_BUFFER_SIZE_BYTES = 1024 * 1024;
 
 	// Defaults for entity creation.
@@ -441,6 +445,32 @@ public class ResourceLoader
 			
 			Supplier<SuspendedCuboid<CuboidData>> dataReader;
 			if (VERSION_CUBOID == version)
+			{
+				dataReader = () -> {
+					CuboidData cuboid = _background_readCuboid(address, buffer);
+					
+					// Load any creatures associated with the cuboid.
+					List<CreatureEntity> creatures = _background_readCreatures(buffer);
+					
+					// Now, load any suspended mutations.
+					List<ScheduledMutation> pendingMutations = _background_readMutations(buffer);
+					// ... and any periodic mutations.
+					Map<BlockAddress, Long> periodicMutations = _background_readPeriodic(buffer);
+					
+					// This should be fully read.
+					Assert.assertTrue(!buffer.hasRemaining());
+					
+					// The height map is ephemeral so it is built here.  Note that building this might be somewhat expensive.
+					CuboidHeightMap heightMap = HeightMapHelpers.buildHeightMap(cuboid);
+					return new SuspendedCuboid<>(cuboid
+							, heightMap
+							, creatures
+							, pendingMutations
+							, periodicMutations
+					);
+				};
+			}
+			else if (VERSION_CUBOID_V7 == version)
 			{
 				dataReader = () -> {
 					CuboidData cuboid = _background_readCuboid(address, buffer);
@@ -949,6 +979,7 @@ public class ResourceLoader
 					|| (VERSION_ENTITY_V4 == version)
 					|| (VERSION_ENTITY_V5 == version)
 					|| (VERSION_ENTITY_V6 == version)
+					|| (VERSION_ENTITY_V7 == version)
 			)
 			{
 				// Do nothing special - just stops old versions from being broken.
