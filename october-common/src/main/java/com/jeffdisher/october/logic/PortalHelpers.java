8d5cbbb0ae66f8ae1dc6743596726b0429f86794
Jeff Disher <jeff@jeffdisher.com>
2025-08-21 01:06:35 -0400

    Created new portal helpers for keystone periodic updates
    -these are not yet used but this introduces the helpers with tests to demonstrate how they are to be used
    -the eventual plan is for the portal keystone to receive periodic mutation updates which it will use to validate the frame and then create or destroy the portal surface multi-block, correspondingly

---
+++ october-common/src/main/java/com/jeffdisher/october/logic/PortalHelpers.java
@@ -0,0 +1,102 @@
+package com.jeffdisher.october.logic;
+
+import com.jeffdisher.october.aspects.Environment;
+import com.jeffdisher.october.aspects.FlagsAspect;
+import com.jeffdisher.october.aspects.OrientationAspect;
+import com.jeffdisher.october.data.BlockProxy;
+import com.jeffdisher.october.data.IMutableBlockProxy;
+import com.jeffdisher.october.mutations.MultiBlockUtils;
+import com.jeffdisher.october.properties.PropertyRegistry;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.Block;
+import com.jeffdisher.october.types.ItemSlot;
+import com.jeffdisher.october.types.NonStackableItem;
+import com.jeffdisher.october.types.TickProcessingContext;
+
+
+/**
+ * Helpers related to the periodic updates for the portal keystone.
+ * Note that much of this will need to be hardened once users can create their own portals.  For now, this just manages
+ * the creation/destruction of the portal surface when the portal composite structure changes.
+ */
+public class PortalHelpers
+{
+	public static final String PORTAL_KEYSTONE_ID = "op.portal_keystone";
+	public static final String PORTAL_SURFACE_ID = "op.portal_surface";
+	public static final String PORTAL_ORB_ID = "op.portal_orb";
+
+	/**
+	 * Checks that the given proxy is currently a portal keystone.
+	 * 
+	 * @param proxy The proxy for the block to check.
+	 * @return True if this is a portal keystone.
+	 */
+	public static boolean isKeystone(IMutableBlockProxy proxy)
+	{
+		return PORTAL_KEYSTONE_ID.equals(proxy.getBlock().item().id());
+	}
+
+	/**
+	 * Called to handle the updates to the portal surface (creating or destroying it) when the keystone receives a
+	 * periodic event (so, this is currently a polling solution).
+	 * NOTE:  MUST be called on a portal keystone.
+	 * 
+	 * @param env The environment.
+	 * @param context The current tick context.
+	 * @param keystoneLocation The location of the keystone.
+	 * @param keystoneProxy The proxy for the keystone.
+	 */
+	public static void handlePortalSurface(Environment env, TickProcessingContext context, AbsoluteLocation keystoneLocation, IMutableBlockProxy keystoneProxy)
+	{
+		AbsoluteLocation surfaceRootLocation = keystoneLocation.getRelative(0, 0, 1);
+		BlockProxy rootProxy = context.previousBlockLookUp.apply(surfaceRootLocation);
+		
+		if (null != rootProxy)
+		{
+			boolean isPortalSurfaceActive = PORTAL_SURFACE_ID.equals(rootProxy.getBlock().item().id());
+			AbsoluteLocation activeTarget = _checkActiveTarget(keystoneProxy);
+			
+			if (null != activeTarget)
+			{
+				// Note that we currently don't check this output location but we assume it is a remote keystone (may
+				// need to check in the future - could be solid blocks which isn't ideal).
+				// We need to create, or maintain, the portal surface.
+				if (!isPortalSurfaceActive)
+				{
+					Block portalSurface = env.blocks.fromItem(env.items.getItemById(PORTAL_SURFACE_ID));
+					OrientationAspect.Direction orientation = keystoneProxy.getOrientation();
+					int entityId = 0;
+					MultiBlockUtils.send2PhaseMultiBlock(env, context, portalSurface, surfaceRootLocation, orientation, entityId);
+				}
+			}
+			else
+			{
+				// We need to break the portal surface, if it is here.
+				if (isPortalSurfaceActive)
+				{
+					// This is always replacing with air
+					Block portalSurface = env.blocks.fromItem(env.items.getItemById(PORTAL_SURFACE_ID));
+					MultiBlockUtils.replaceMultiBlock(env, context, surfaceRootLocation, portalSurface, env.special.AIR);
+				}
+			}
+		}
+		else
+		{
+			// NOTE:  For now, we ignore this when not loaded.
+		}
+	}
+
+
+	private static AbsoluteLocation _checkActiveTarget(IMutableBlockProxy keystoneProxy)
+	{
+		byte flags = keystoneProxy.getFlags();
+		AbsoluteLocation target = null;
+		if (FlagsAspect.isSet(flags, FlagsAspect.FLAG_ACTIVE))
+		{
+			ItemSlot slot = keystoneProxy.getSpecialSlot();
+			NonStackableItem nonStack = (null != slot) ? slot.nonStackable : null;
+			target = (null != nonStack) ? (AbsoluteLocation) nonStack.properties().get(PropertyRegistry.LOCATION) : null;
+		}
+		return target;
+	}
+}
---
+++ october-common/src/test/java/com/jeffdisher/october/logic/TestPortalHelpers.java
@@ -0,0 +1,178 @@
+package com.jeffdisher.october.logic;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jeffdisher.october.aspects.AspectRegistry;
+import com.jeffdisher.october.aspects.Environment;
+import com.jeffdisher.october.aspects.FlagsAspect;
+import com.jeffdisher.october.data.BlockProxy;
+import com.jeffdisher.october.data.CuboidData;
+import com.jeffdisher.october.data.MutableBlockProxy;
+import com.jeffdisher.october.mutations.IMutationBlock;
+import com.jeffdisher.october.mutations.MutationBlockPhase2Multi;
+import com.jeffdisher.october.mutations.MutationBlockPlaceMultiBlock;
+import com.jeffdisher.october.mutations.MutationBlockReplace;
+import com.jeffdisher.october.properties.PropertyRegistry;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.Block;
+import com.jeffdisher.october.types.ContextBuilder;
+import com.jeffdisher.october.types.CuboidAddress;
+import com.jeffdisher.october.types.Item;
+import com.jeffdisher.october.types.ItemSlot;
+import com.jeffdisher.october.types.NonStackableItem;
+import com.jeffdisher.october.types.TickProcessingContext;
+import com.jeffdisher.october.utils.CuboidGenerator;
+
+
+public class TestPortalHelpers
+{
+	private static Environment ENV;
+	private static Item ORB_ITEM;
+	private static Block VOID_STONE;
+	private static Block KEYSTONE;
+	private static Block PORTAL_SURFACE;
+	@BeforeClass
+	public static void setup()
+	{
+		ENV = Environment.createSharedInstance();
+		ORB_ITEM = ENV.items.getItemById("op.portal_orb");
+		VOID_STONE = ENV.blocks.fromItem(ENV.items.getItemById("op.void_stone"));
+		KEYSTONE = ENV.blocks.fromItem(ENV.items.getItemById("op.portal_keystone"));
+		PORTAL_SURFACE = ENV.blocks.fromItem(ENV.items.getItemById("op.portal_surface"));
+	}
+	@AfterClass
+	public static void tearDown()
+	{
+		Environment.clearSharedInstance();
+	}
+
+	@Test
+	public void keystone()
+	{
+		// Checks that the keystone type check helper works.
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(CuboidAddress.fromInt(0, 0, 0), ENV.special.AIR);
+		AbsoluteLocation voidStone = cuboid.getCuboidAddress().getBase().getRelative(16, 16, 16);
+		AbsoluteLocation keystoneLocation = voidStone.getRelative(1, 0, 0);
+		cuboid.setData15(AspectRegistry.BLOCK, voidStone.getBlockAddress(), VOID_STONE.item().number());
+		cuboid.setData15(AspectRegistry.BLOCK, keystoneLocation.getBlockAddress(), KEYSTONE.item().number());
+		
+		MutableBlockProxy proxy = new MutableBlockProxy(voidStone, cuboid);
+		Assert.assertFalse(PortalHelpers.isKeystone(proxy));
+		proxy = new MutableBlockProxy(keystoneLocation, cuboid);
+		Assert.assertTrue(PortalHelpers.isKeystone(proxy));
+	}
+
+	@Test
+	public void handleActionSurfaceNoChange() throws Throwable
+	{
+		CuboidAddress cuboidAddress = CuboidAddress.fromInt(0, 0, 0);
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(cuboidAddress, ENV.special.AIR);
+		NonStackableItem portalOrb = new NonStackableItem(ORB_ITEM, Map.of(PropertyRegistry.LOCATION, cuboidAddress.getBase()));
+		AbsoluteLocation keystoneLocation = new AbsoluteLocation(1, 2, 3);
+		AbsoluteLocation surfaceBase = keystoneLocation.getRelative(0, 0, 1);
+		cuboid.setData15(AspectRegistry.BLOCK, keystoneLocation.getBlockAddress(), KEYSTONE.item().number());
+		cuboid.setData7(AspectRegistry.FLAGS, keystoneLocation.getBlockAddress(), FlagsAspect.FLAG_ACTIVE);
+		cuboid.setDataSpecial(AspectRegistry.SPECIAL_ITEM_SLOT, keystoneLocation.getBlockAddress(), ItemSlot.fromNonStack(portalOrb));
+		cuboid.setData15(AspectRegistry.BLOCK, surfaceBase.getBlockAddress(), PORTAL_SURFACE.item().number());
+		
+		TickProcessingContext context = ContextBuilder.build()
+			.lookups((AbsoluteLocation location) -> {
+				return new BlockProxy(location.getBlockAddress(), cuboid);
+			}, null)
+			.finish()
+		;
+		
+		// This should do nothing since everything is in the right shape.
+		MutableBlockProxy proxy = new MutableBlockProxy(keystoneLocation, cuboid);
+		PortalHelpers.handlePortalSurface(ENV, context, keystoneLocation, proxy);
+	}
+
+	@Test
+	public void handleActionCreateSurface() throws Throwable
+	{
+		CuboidAddress cuboidAddress = CuboidAddress.fromInt(0, 0, 0);
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(cuboidAddress, ENV.special.AIR);
+		NonStackableItem portalOrb = new NonStackableItem(ORB_ITEM, Map.of(PropertyRegistry.LOCATION, cuboidAddress.getBase()));
+		AbsoluteLocation keystoneLocation = new AbsoluteLocation(1, 2, 3);
+		cuboid.setData15(AspectRegistry.BLOCK, keystoneLocation.getBlockAddress(), KEYSTONE.item().number());
+		cuboid.setData7(AspectRegistry.FLAGS, keystoneLocation.getBlockAddress(), FlagsAspect.FLAG_ACTIVE);
+		cuboid.setDataSpecial(AspectRegistry.SPECIAL_ITEM_SLOT, keystoneLocation.getBlockAddress(), ItemSlot.fromNonStack(portalOrb));
+		
+		Set<AbsoluteLocation> nextLocations = new HashSet<>();
+		Set<AbsoluteLocation> futureLocations = new HashSet<>();
+		TickProcessingContext context = ContextBuilder.build()
+			.lookups((AbsoluteLocation location) -> {
+				return new BlockProxy(location.getBlockAddress(), cuboid);
+			}, null)
+			.sinks(new TickProcessingContext.IMutationSink() {
+				@Override
+				public void next(IMutationBlock mutation)
+				{
+					Assert.assertTrue(mutation instanceof MutationBlockPlaceMultiBlock);
+					boolean didAdd = nextLocations.add(mutation.getAbsoluteLocation());
+					Assert.assertTrue(didAdd);
+				}
+				@Override
+				public void future(IMutationBlock mutation, long millisToDelay)
+				{
+					Assert.assertTrue(mutation instanceof MutationBlockPhase2Multi);
+					Assert.assertEquals(ContextBuilder.DEFAULT_MILLIS_PER_TICK, millisToDelay);
+					boolean didAdd = futureLocations.add(mutation.getAbsoluteLocation());
+					Assert.assertTrue(didAdd);
+				}
+			}, null)
+			.finish()
+		;
+		
+		// We should see the 2-phase commits.
+		MutableBlockProxy proxy = new MutableBlockProxy(keystoneLocation, cuboid);
+		PortalHelpers.handlePortalSurface(ENV, context, keystoneLocation, proxy);
+		Assert.assertEquals(9, nextLocations.size());
+		Assert.assertEquals(9, futureLocations.size());
+	}
+
+	@Test
+	public void handleActionDestroySurface() throws Throwable
+	{
+		CuboidAddress cuboidAddress = CuboidAddress.fromInt(0, 0, 0);
+		CuboidData cuboid = CuboidGenerator.createFilledCuboid(cuboidAddress, ENV.special.AIR);
+		AbsoluteLocation keystoneLocation = new AbsoluteLocation(1, 2, 3);
+		AbsoluteLocation surfaceBase = keystoneLocation.getRelative(0, 0, 1);
+		cuboid.setData15(AspectRegistry.BLOCK, keystoneLocation.getBlockAddress(), KEYSTONE.item().number());
+		cuboid.setData15(AspectRegistry.BLOCK, surfaceBase.getBlockAddress(), PORTAL_SURFACE.item().number());
+		
+		Set<AbsoluteLocation> nextLocations = new HashSet<>();
+		TickProcessingContext context = ContextBuilder.build()
+			.lookups((AbsoluteLocation location) -> {
+				return new BlockProxy(location.getBlockAddress(), cuboid);
+			}, null)
+			.sinks(new TickProcessingContext.IMutationSink() {
+				@Override
+				public void next(IMutationBlock mutation)
+				{
+					Assert.assertTrue(mutation instanceof MutationBlockReplace);
+					boolean didAdd = nextLocations.add(mutation.getAbsoluteLocation());
+					Assert.assertTrue(didAdd);
+				}
+				@Override
+				public void future(IMutationBlock mutation, long millisToDelay)
+				{
+					Assert.fail();
+				}
+			}, null)
+			.finish()
+		;
+		
+		// We should see the replace calls to break the surface.
+		MutableBlockProxy proxy = new MutableBlockProxy(keystoneLocation, cuboid);
+		PortalHelpers.handlePortalSurface(ENV, context, keystoneLocation, proxy);
+		Assert.assertEquals(9, nextLocations.size());
+	}
+}
