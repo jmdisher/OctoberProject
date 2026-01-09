471fe34b80185c51469a19843a94a05ad230716f
Jeff Disher <jeff@jeffdisher.com>
2026-01-09 13:31:47 -0500

    Created a utility to find the closest block of a specific type to a location
    -this will be connected to a console command, in a later commit, but is a general useful utility

--- october-common/src/main/java/com/jeffdisher/october/logic/MiscHelpers.java
+++ october-common/src/main/java/com/jeffdisher/october/logic/MiscHelpers.java
@@ -5,8 +5,15 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import com.jeffdisher.october.aspects.AspectRegistry;
 import com.jeffdisher.october.aspects.Environment;
+import com.jeffdisher.october.data.IOctree;
+import com.jeffdisher.october.data.IReadOnlyCuboidData;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.Block;
+import com.jeffdisher.october.types.BlockAddress;
 import com.jeffdisher.october.types.DropChance;
 import com.jeffdisher.october.types.Item;
 import com.jeffdisher.october.types.ItemSlot;
@@ -81,4 +88,84 @@ public class MiscHelpers
 		return slots;
 	}
 
+	/**
+	 * A utility method used to find the closest instance of a block of type to the given centre location by searching
+	 * the given set of cuboids.
+	 * 
+	 * @param cuboids The cuboids to search.
+	 * @param centre Find the closest block to this centre of the search area.
+	 * @param type The block type.
+	 * @return The location of the nearest block of the given type, or null if there isn't one.
+	 */
+	public static AbsoluteLocation findClosestBlock(Set<IReadOnlyCuboidData> cuboids, AbsoluteLocation centre, Block type)
+	{
+		AbsoluteLocation[] outParam = new AbsoluteLocation[1];
+		AbsoluteLocation[] cuboidBase = new AbsoluteLocation[1];
+		
+		short blockToMatch = type.item().number();
+		IOctree.IWalkerCallback<Short> walker = new IOctree.IWalkerCallback<>() {
+			int squareDistance = Integer.MAX_VALUE;
+			@Override
+			public void visit(BlockAddress base, byte size, Short value)
+			{
+				if (blockToMatch == value)
+				{
+					AbsoluteLocation absoluteBase = cuboidBase[0].relativeForBlock(base);
+					AbsoluteLocation inBase = _getClosest(centre, absoluteBase, size);
+					int deltaX = centre.x() - inBase.x();
+					int deltaY = centre.y() - inBase.y();
+					int deltaZ = centre.z() - inBase.z();
+					int thisDistance = deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ;
+					if (thisDistance < squareDistance)
+					{
+						squareDistance = thisDistance;
+						outParam[0] = inBase;
+					}
+				}
+			}
+		};
+		
+		// We need to skip something so just make it the next value.
+		short valueToSkip = (short)(blockToMatch + 1);
+		for (IReadOnlyCuboidData cuboid : cuboids)
+		{
+			cuboidBase[0] = cuboid.getCuboidAddress().getBase();
+			cuboid.walkData(AspectRegistry.BLOCK, walker, valueToSkip);
+		}
+		return outParam[0];
+	}
+
+
+	private static AbsoluteLocation _getClosest(AbsoluteLocation centre, AbsoluteLocation absoluteBase, byte size)
+	{
+		int lowX = absoluteBase.x();
+		int highX = lowX + size  - 1;
+		int lowY = absoluteBase.y();
+		int highY = lowY + size  - 1;
+		int lowZ = absoluteBase.z();
+		int highZ = lowZ + size  - 1;
+		
+		int inX = _closestOneDimension(centre.x(), lowX, highX);
+		int inY = _closestOneDimension(centre.y(), lowY, highY);
+		int inZ = _closestOneDimension(centre.z(), lowZ, highZ);
+		return new AbsoluteLocation(inX, inY, inZ);
+	}
+
+	public static int _closestOneDimension(int search, int low, int high)
+	{
+		int in;
+		if (search < low)
+		{
+			in = low;
+		}
+		else if (search > high)
+		{
+			in = high;
+		}
+		else
+		{
+			in = search;
+		}
+		return in;
+	}
 }
--- october-common/src/test/java/com/jeffdisher/october/logic/TestMiscHelpers.java
+++ october-common/src/test/java/com/jeffdisher/october/logic/TestMiscHelpers.java
@@ -1,14 +1,23 @@
 package com.jeffdisher.october.logic;
 
+import java.util.Set;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.jeffdisher.october.aspects.AspectRegistry;
 import com.jeffdisher.october.aspects.Environment;
+import com.jeffdisher.october.data.CuboidData;
+import com.jeffdisher.october.types.AbsoluteLocation;
+import com.jeffdisher.october.types.Block;
+import com.jeffdisher.october.types.BlockAddress;
+import com.jeffdisher.october.types.CuboidAddress;
 import com.jeffdisher.october.types.DropChance;
 import com.jeffdisher.october.types.Item;
 import com.jeffdisher.october.types.ItemSlot;
+import com.jeffdisher.october.utils.CuboidGenerator;
 
 
 public class TestMiscHelpers
@@ -77,5 +86,22 @@ public class TestMiscHelpers
 		Assert.assertEquals(2, slot30.length);
 		Assert.assertEquals(1, slot50.length);
 		Assert.assertEquals(1, slot99.length);
+	}
+
+	@Test
+	public void nearestBlock() throws Throwable
+	{
+		Block stone = ENV.blocks.fromItem(ENV.items.getItemById("op.stone"));
+		CuboidData airCuboid = CuboidGenerator.createFilledCuboid(CuboidAddress.fromInt(0, 0, 0), ENV.special.AIR);
+		CuboidData stoneCuboid = CuboidGenerator.createFilledCuboid(CuboidAddress.fromInt(0, 0, -1), stone);
+		CuboidData mixedCuboid = CuboidGenerator.createFilledCuboid(CuboidAddress.fromInt(1, 0, 0), ENV.special.AIR);
+		mixedCuboid.setData15(AspectRegistry.BLOCK, BlockAddress.fromInt(0, 0, 0), stone.item().number());
+		mixedCuboid.setData15(AspectRegistry.BLOCK, BlockAddress.fromInt(31, 31, 31), stone.item().number());
+		
+		Assert.assertEquals(new AbsoluteLocation(5, 6, -1), MiscHelpers.findClosestBlock(Set.of(airCuboid, stoneCuboid, mixedCuboid), new AbsoluteLocation(5, 6, 7), stone));
+		Assert.assertEquals(new AbsoluteLocation(5, 6, 7), MiscHelpers.findClosestBlock(Set.of(airCuboid, stoneCuboid, mixedCuboid), new AbsoluteLocation(5, 6, 7), ENV.special.AIR));
+		Assert.assertNull(MiscHelpers.findClosestBlock(Set.of(airCuboid), new AbsoluteLocation(5, 6, 7), stone));
+		Assert.assertEquals(new AbsoluteLocation(31, 8, -1), MiscHelpers.findClosestBlock(Set.of(airCuboid, stoneCuboid, mixedCuboid), new AbsoluteLocation(37, 8, 9), stone));
+		Assert.assertEquals(new AbsoluteLocation(32, 0, 0), MiscHelpers.findClosestBlock(Set.of(mixedCuboid), new AbsoluteLocation(37, 8, 9), stone));
 	}
 }
