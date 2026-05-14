f74ee897682d74ffa7dba0035a7cb7122c3162c7
Jeff Disher <jeff@jeffdisher.com>
2026-05-14 00:19:16 -0400

    Extracted the common IBlockFetcher logic from WorldContainer as needed elsewhere
    -this same caching logic is needed in OctoberPeaks and potentially some other parts of OctoberProject so this allows reuse where only the per-cuboid data and cache are provided in special ways (via a passed-in Function)

--- october-client/src/main/java/com/jeffdisher/october/client/WorldContainer.java
+++ october-client/src/main/java/com/jeffdisher/october/client/WorldContainer.java
@@ -1,15 +1,13 @@
 package com.jeffdisher.october.client;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 
-import com.jeffdisher.october.aspects.AspectRegistry;
 import com.jeffdisher.october.data.BlockProxy;
 import com.jeffdisher.october.data.CuboidData;
 import com.jeffdisher.october.data.CuboidHeightMap;
@@ -23,6 +21,7 @@ import com.jeffdisher.october.types.CuboidColumnAddress;
 import com.jeffdisher.october.types.Pair;
 import com.jeffdisher.october.types.TickProcessingContext;
 import com.jeffdisher.october.utils.Assert;
+import com.jeffdisher.october.utils.CommonBlockFetcher;
 
 
 /**
@@ -35,7 +34,7 @@ public class WorldContainer
 	private final Map<CuboidAddress, _Tuple> _worldCuboids = new HashMap<>();
 	private final Map<CuboidColumnAddress, Map<Short, _Tuple>> _worldColumns = new HashMap<>();
 	private final Map<CuboidAddress, _Tuple> _projectedCuboids = new HashMap<>();
-	public final TickProcessingContext.IBlockFetcher blockFetcher = new _BlockFetcher();
+	public final TickProcessingContext.IBlockFetcher blockFetcher = new CommonBlockFetcher(new _DataFetcher());
 
 	/**
 	 * @return The set of all cuboid addresses known to the container.
@@ -284,98 +283,10 @@ public class WorldContainer
 		public Map<BlockAddress, BlockProxy> projectedCache;
 	}
 
-	private class _BlockFetcher implements TickProcessingContext.IBlockFetcher
+	private class _DataFetcher implements Function<CuboidAddress, Pair<IReadOnlyCuboidData, Map<BlockAddress, BlockProxy>>>
 	{
 		@Override
-		public BlockProxy readBlock(AbsoluteLocation location)
-		{
-			BlockProxy proxy = null;
-			
-			Pair<IReadOnlyCuboidData, Map<BlockAddress, BlockProxy>> pair = _getMutableCacheForCuboid(location.getCuboidAddress());
-			if (null != pair)
-			{
-				Map<BlockAddress, BlockProxy> cache = pair.two();
-				BlockAddress blockAddress = location.getBlockAddress();
-				proxy = cache.get(blockAddress);
-				if (null == proxy)
-				{
-					proxy = BlockProxy.load(blockAddress, pair.one());
-					cache.put(blockAddress, proxy);
-				}
-			}
-			return proxy;
-		}
-		@Override
-		public Map<AbsoluteLocation, BlockProxy> readBlockBatch(Collection<AbsoluteLocation> locations)
-		{
-			// Collect the per-cuboid lookups, first.
-			Map<CuboidAddress, List<BlockAddress>> split = _splitLocationsByCuboid(locations);
-			return _fetchProxiesByCuboid(split);
-		}
-		private Map<CuboidAddress, List<BlockAddress>> _splitLocationsByCuboid(Collection<AbsoluteLocation> locations)
-		{
-			Map<CuboidAddress, List<BlockAddress>> split = new HashMap<>();
-			for (AbsoluteLocation loc : locations)
-			{
-				CuboidAddress cuboidAddress = loc.getCuboidAddress();
-				List<BlockAddress> list = split.get(cuboidAddress);
-				if (null == list)
-				{
-					list = new ArrayList<>();
-					split.put(cuboidAddress, list);
-				}
-				list.add(loc.getBlockAddress());
-			}
-			return split;
-		}
-		private Map<AbsoluteLocation, BlockProxy> _fetchProxiesByCuboid(Map<CuboidAddress, List<BlockAddress>> split)
-		{
-			Map<AbsoluteLocation, BlockProxy> result = new HashMap<>();
-			for (Map.Entry<CuboidAddress, List<BlockAddress>> elt : split.entrySet())
-			{
-				CuboidAddress cuboidAddress = elt.getKey();
-				Pair<IReadOnlyCuboidData, Map<BlockAddress, BlockProxy>> pair = _getMutableCacheForCuboid(cuboidAddress);
-				if (null != pair)
-				{
-					AbsoluteLocation base = cuboidAddress.getBase();
-					List<BlockAddress> toLoad = new ArrayList<>();
-					Map<BlockAddress, BlockProxy> cache = pair.two();
-					for (BlockAddress blockAddress : elt.getValue())
-					{
-						BlockProxy proxy = cache.get(blockAddress);
-						if (null != proxy)
-						{
-							AbsoluteLocation location = base.relativeForBlock(blockAddress);
-							result.put(location, proxy);
-						}
-						else
-						{
-							toLoad.add(blockAddress);
-						}
-					}
-					if (!toLoad.isEmpty())
-					{
-						BlockAddress[] array = toLoad.toArray((int size) -> new BlockAddress[size]);
-						IReadOnlyCuboidData.BlockAddressBatchComparator comparator = new IReadOnlyCuboidData.BlockAddressBatchComparator();
-						Arrays.sort(array, comparator);
-						IReadOnlyCuboidData cuboid = pair.one();
-						short[] blocks = cuboid.batchReadData15(AspectRegistry.BLOCK, array);
-						for (int i = 0; i < array.length; ++i)
-						{
-							short number = blocks[i];
-							BlockAddress blockAddress = array[i];
-							BlockProxy proxy = BlockProxy.init(blockAddress, cuboid, number);
-							cache.put(blockAddress, proxy);
-							
-							AbsoluteLocation location = base.relativeForBlock(blockAddress);
-							result.put(location, proxy);
-						}
-					}
-				}
-			}
-			return result;
-		}
-		private Pair<IReadOnlyCuboidData, Map<BlockAddress, BlockProxy>> _getMutableCacheForCuboid(CuboidAddress cuboidAddress)
+		public Pair<IReadOnlyCuboidData, Map<BlockAddress, BlockProxy>> apply(CuboidAddress cuboidAddress)
 		{
 			// We will prioritize the projected cache, if there is one and associated data, but otherwise use shadow.
 			Pair<IReadOnlyCuboidData, Map<BlockAddress, BlockProxy>> pair = null;
